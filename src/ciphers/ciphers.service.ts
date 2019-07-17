81afdf66a896808ee4ca5d4468ce55197c7a138f
Russ yev <ruslanyev@yahoo.com>
2019-07-17 15:50:17 -0700

    added a GET and ADD methods

--- src/ciphers/ciphers.service.ts
+++ src/ciphers/ciphers.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+import { CIPHERS } from '../mocks/ciphers.mock';
 
 @Injectable()
-export class CiphersService {}
+export class CiphersService {
+  ciphers = CIPHERS;
+
+  getCiphers(): Promise<any> {
+    return new Promise(resolve => {
+      resolve(this.ciphers);
+    });
+  }
+
+  addCipher(cipher): Promise<any> {
+    return new Promise(resolve => {
+      this.ciphers.push(cipher);
+      resolve(this.ciphers);
+    });
+  }
+}
