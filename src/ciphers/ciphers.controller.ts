33b5d0b7486eee1cf2ef197d8df8737e83eecba1
Russ yev <ruslanyev@yahoo.com>
2019-07-17 16:09:42 -0700

    added service into controller

--- src/ciphers/ciphers.controller.ts
+++ src/ciphers/ciphers.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CiphersService } from './ciphers.service'
+import { CreateCipherDTO } from './dto/create-cipher.dto'
 
 @Controller('ciphers')
-export class CiphersController {}
+export class CiphersController {
+  constructor(private ciphersService: CiphersService) { }
+
+    @Get()
+    async getCiphers() {
+        const books = await this.ciphersService.getCiphers();
+        return books;
+    }
+
+    @Post()
+    async addCipher(@Body() createCipherDTO: CreateCipherDTO) {
+        const book = await this.ciphersService.addCipher(createCipherDTO);
+        return book;
+    }
+}
