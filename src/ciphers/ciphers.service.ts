0a824c77e641ae1d4770cfeb9daabb10166cd59d
Russ yev <ruslanyev@yahoo.com>
2019-07-17 18:26:21 -0700

    added Validation

--- src/ciphers/ciphers.service.ts
+++ src/ciphers/ciphers.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, HttpException } from '@nestjs/common';
 import { CIPHERS } from '../mocks/ciphers.mock';
+import { caesarShift } from '../functions/caesarShift';
 
 @Injectable()
 export class CiphersService {
@@ -12,9 +13,37 @@ export class CiphersService {
   }
 
   addCipher(cipher): Promise<any> {
+    const requiredFields = ['toShift', 'shiftAmount'];
+    const missingField = requiredFields.find(field => !(field in cipher));
+    const aNumber = typeof cipher.shiftAmount === 'number';
+    const aString = typeof cipher.toShift === 'string';
+
     return new Promise(resolve => {
-      this.ciphers.push(cipher);
-      resolve(this.ciphers);
+      if (missingField) {
+        throw new HttpException(
+          `You're missing the field ${missingField}`,
+          422,
+        );
+      }
+
+      if (!aNumber) {
+        throw new HttpException('Shift amount is not a number', 422);
+      }
+
+      if (!aString) {
+        throw new HttpException('toShift is not a string', 422);
+      }
+
+      const afterShift = caesarShift(cipher.toShift, cipher.shiftAmount);
+
+      const newCaesarCipher = {
+        toShift: cipher.toShift,
+        afterShift,
+        shiftAmount: cipher.shiftAmount,
+      };
+
+      this.ciphers.unshift(newCaesarCipher);
+      resolve(newCaesarCipher);
     });
   }
 }
